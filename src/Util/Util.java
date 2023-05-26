26aefd0d269af32fa378f4774101518338ec08b1
Adulescentia <sonsw2422@gmail.com>
2023-05-27 01:02:21 +0900

    drawCertainAmount(특정개수 순차적으로 빼오는거 추가)(쓸모無)

--- src/Util/Util.java
+++ src/Util/Util.java
@@ -9,6 +9,12 @@ public class Util {
     public static void printf(String format,Object... args){ out.printf(format,args); }
     public static void println(Object o){ out.println(o); }
     public static void print(Object o){ out.print(o); }
-
-    public static <T> ArrayList<T> ListOf (T... elements){return new ArrayList<T>(Arrays.asList(elements));}
+    public static <T> ArrayList<T> listOf (T... elements){return new ArrayList<T>(Arrays.asList(elements));}
+    public static <T> ArrayList<T> drawCertainAmount (ArrayList<T> array,int amount){
+        ArrayList<T> returnList = new ArrayList<T>();
+        for(int i = 0; i<amount; i++){
+            returnList.add(array.get(i));
+        }
+        return returnList;
+    }
 }
